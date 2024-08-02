d45050c7859aa2113683f9cfa02ad3a165cfbab0
Franck Pommereau <franck.pommereau@gmail.com>
2024-08-02 10:56:05 +0200

    made test_daddy.py more verbose to avoid silent fails

--- test_daddy.py
+++ test_daddy.py
@@ -2,5 +2,7 @@ import doctest
 import daddy
 import daddy.dddlib
 
-doctest.testmod(daddy)
-doctest.testmod(daddy.dddlib)
+for mod in (daddy, daddy.dddlib):
+    print(f"testing '{mod.__name__}'")
+    f, c = doctest.testmod(mod)
+    print(f"> performed {c} tests, {f} failed")
